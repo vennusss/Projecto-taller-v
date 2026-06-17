4caa255e4ac5c445b2781501f4f994b9db58643d
Gabrielit123 <gabrielpobletecastillo@gmail.com>
2026-06-17 00:12:05 -0400

    Lo logre mami

--- demostracion3.cpp
+++ demostracion3.cpp
@@ -2,19 +2,43 @@
 #include <fstream>
 
 void arbol::cargarDiccionario(vector<string>& palabras, int l, int ri){
+    cargarDiccionario(palabras, l, ri, &R);
+}
+
+void arbol::cargarDiccionario(vector<string>& palabras, int l, int ri, nodoT** nodo){
     if (l > ri) return;
+    int n = ri - l + 1;
 
-    int m = (l + ri) / 2;
+    //  si los elementos del array son menor a la separación => inserta directamente
+    if (n <= K - 1){
+        for (int i = l; i <= ri; i++)
+            insert(nodo, palabras[i]);
+        return;
+    }
 
-    // Insertar la palabra del medio primero
-    insert(&R, palabras[m]);
+    // calcular los K-1 separadores
+    vector<int> sep(K - 1);
+    for (int i = 0; i < K - 1; i++)
+        sep[i] = l + ((i + 1) * n) / K;  // (i+1)*n/K evita error acumulado
 
-    // Luego recursivamente la mitad izquierda y derecha
-    cargarDiccionario(palabras, l, m - 1);
-    cargarDiccionario(palabras, m + 1, ri);
+    // insertar los K-1 separadores en el nodo actual
+    for (int i = 0; i < K - 1; i++)
+        insert(nodo, palabras[sep[i]]);
+
+    // recursar en los K subrangos hacia cada hijo
+    // subrango 0: [l, sep[0]-1]
+    cargarDiccionario(palabras, l, sep[0] - 1, &((*nodo)->hijos[0]));
+
+    // subrangos intermedios: [sep[i]+1, sep[i+1]-1]
+    for (int i = 0; i < K - 2; i++)
+        cargarDiccionario(palabras, sep[i] + 1, sep[i + 1] - 1, &((*nodo)->hijos[i + 1]));
+
+    // subrango final: [sep[K-2]+1, ri]
+    cargarDiccionario(palabras, sep[K - 2] + 1, ri, &((*nodo)->hijos[K - 1]));
 }
 int main(){
     arbol sudowodo;
+    
     vector<string> palabras;
     string palabra;
 
@@ -26,7 +50,6 @@ int main(){
 
     // Cargar en el árbol de forma balanceada
     sudowodo.cargarDiccionario(palabras, 0, palabras.size() - 1);
-    sudowodo.print();
     cout << "cantidad de nodos: " << sudowodo.getCantN() << endl;
     cout << "Peso del arbol: " << sizeof(nodoT) << endl;
     
